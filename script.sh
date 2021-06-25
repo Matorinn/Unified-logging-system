2f027c77f6fa2437c01dc6ab95a5e0ef23449571
Daniel <dany.lesckovetz@gmail.com>
2021-06-25 11:22:50 +0700

    Changa script.sh path

--- script.sh
+++ script.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-uls=/home/$(whoami)/Desktop/Unified-logging-system/bin/uls
+uls=bin/uls
 
 make clean && make
 
 $uls first set configfile bin/first.conf
-$uls example set configfile /home/$(whoami)/Desktop/Unified-logging-system/src/signal_for_zlog/example.conf
+$uls example set configfile src/signal_for_zlog/example.conf
 $uls third set configfile bin/third.conf
 $uls example set logfile bin/EXAMPLE.LOG
 $uls example set logfile /home/$(whoami)/Desktop/example.log
